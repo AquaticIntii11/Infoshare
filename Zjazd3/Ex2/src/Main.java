734dfa4ac1b52095990defed064e79b6b76b1220
AquaticIntii11 <aquatic.intii@gmail.com>
2018-06-17 13:58:20 +0200

    Adding ex2

--- Zjazd3/Ex2/src/Main.java
+++ Zjazd3/Ex2/src/Main.java
@@ -2,11 +2,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ProcessText sentence = new ProcessText();
 
+        //Count dots
+        ProcessText sentence = new ProcessText();
         Scanner in = new Scanner(System.in);
         String text = in.nextLine(); //"This is a sentence with some dots. We like dots. Very much."
         System.out.println("Provide some text:");
         sentence.countDots(text);
+
+        //Abstract clases
+        User newUser = new Admin();
+        newUser.showInfo();
     }
 }
